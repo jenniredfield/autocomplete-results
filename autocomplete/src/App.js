6c074bcbfeaf07cde5d67cf83de9fef0395380b0
Jen Bird <jenniredfield@hotmail.com>
2020-01-11 11:50:39 +0000

    Request function working

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  getData = (searchTerm) => {
+    axios(`https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=6&solrTerm=${searchTerm}`)
+    .then(res => console.log(res))
+  }
+
+  handleInput = (e) => {
+    const value = e.target.value;
+
+    if(value.length <= 1) {
+      return;
+    }
+
+    this.getData(value);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+          <div className="main__container">
+            <input 
+              placeholder='Search for location...' 
+              onInput={this.handleInput}></input>
+
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
