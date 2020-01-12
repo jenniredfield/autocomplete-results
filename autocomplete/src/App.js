44ffa105ec05d579413cee312df4d2c76484b582
Jen Bird <jenniredfield@hotmail.com>
2020-01-12 13:37:43 +0000

    Merged comments

--- autocomplete/src/App.js
+++ autocomplete/src/App.js
@@ -7,10 +7,12 @@ import './styles/styles.css';
 import axios from 'axios';
 
 const NUMBER_OF_RESULTS = 6;
+
 // make tests
 // check mobile styling
 // add accessibility arias
-
+// remove console logs
+// add linter
 
 class App extends Component {
 
@@ -168,7 +170,6 @@ class App extends Component {
                 : null}
             </ul>
           </div>
-
         </div>
       </div>
     );
